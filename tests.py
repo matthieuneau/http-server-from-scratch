1c0ba2d6464661e3dac505cf12a6435cd9cf39fd
= <=>
2024-12-28 09:37:31 +0000

    add test for request parser

---
+++ tests.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    request = """POST /form HTTP/1.1
+    Host: localhost:8080
+    User-Agent: curl/7.68.0
+    Content-Type: application/x-www-form-urlencoded
+    Content-Length: 13
+
+    name=JohnDoe"""
+    print(parse_request(request))
